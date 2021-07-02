88e2941f10d0efe6d8b739f7d9636289f68c23ff
Donik1984 <86175071+Donik1984@users.noreply.github.com>
2021-07-01 22:49:12 -0400

    End of Day Commit

--- BalancedBrackets/Program.cs
+++ BalancedBrackets/Program.cs
@@ -7,7 +7,7 @@ namespace BalancedBracketsNS
         static void Main(string[] args)
         {
 
-            Console.WriteLine(BalancedBrackets.HasBalancedBrackets("[LaunchCode]"));
+            //Console.WriteLine(BalancedBrackets.HasBalancedBrackets("[LaunchCode]"));
             
 
 
